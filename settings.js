f6078ff005eb829a78b4c981567a2b25a16b6953
masedi666 <129651980+masedi666@users.noreply.github.com>
2023-04-04 04:06:47 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.creator = 'edikur.exe'// yourname
+global.MONGO_DB_URI = "mongodb+srv://edi1:edi1@cluster0.boefhgr.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "edikurexe" //isi apa aja bebas
+global.your_email = "Nawanjr19220@gmail.com" //email
+global.email_password = "nawan123456@" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
